e3868b7eb44be9f8d7adc8e2e03ae88aae0d17f7
Alexander Troshkov <20989940+aednlaxer@users.noreply.github.com>
2024-10-16 15:39:27 +0300

    Remove Flutter from coverage generation

--- scripts/get_test_coverage.sh
+++ scripts/get_test_coverage.sh
@@ -1,3 +1,4 @@
-flutter test --coverage
+dart pub global activate coverage
+dart pub global run coverage:test_with_coverage
 lcov --remove coverage/lcov.info 'lib/src/message_parser/*' 'lib/src/generated/*' -o coverage/new_lcov.info
 genhtml coverage/new_lcov.info --output=coverage
